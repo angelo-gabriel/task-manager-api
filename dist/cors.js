ec0444ed808b813daaf87985c587b1a0beadcd6f
Angelo Gabriel <aangelo6974@gmail.com>
2025-03-04 12:15:52 -0300

    teste

---
+++ dist/cors.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.corsOptions = void 0;
+exports.corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200,
+};
